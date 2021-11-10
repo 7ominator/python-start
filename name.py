b4fc25f7544c5176291cacb5368204f5ed3caafa
7ominator <82764548+7ominator@users.noreply.github.com>
2021-11-10 17:10:02 +0100

    Neu

---
+++ name.py
@@ -0,0 +1,13 @@
+x = str(input("Name:"))
+if x == Tim:
+    print("Timinator")
+elif x == Tom:
+    print("Tominator")
+elif x == Til:
+    print("Belauscher")
+elif x == Ben:
+    print("Benane")
+elif x == Elliot:
+    print("Lmau")
+elif x == Jasper:
+    print("minced")
